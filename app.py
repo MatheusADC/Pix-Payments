fdbf56e5890c76afa047453d377c01673c06e7f5
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-27 11:47:18 -0300

    Add: Dynamic data to the payments page.

--- app.py
+++ app.py
@@ -42,7 +42,12 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-    return render_template('payment.html')
+    payment = Payment.query.get(payment_id)
+    return render_template('payment.html', 
+                           payment_id=payment_id, 
+                           value=payment.value, 
+                           host="YOUR_HOST", 
+                           qr_code=payment.qr_code)
 
 if __name__ == '__main__':
     app.run(debug=True)
